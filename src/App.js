80ec5dc23720d228d77f3cee98174e855247a389
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-10-20 17:58:04 +0700

    import beatufiul-react-diagrams

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { Container } from "./components";
+import "beautiful-react-diagrams/styles.css";
+
 function App() {
 	return (
 		<div className="app">
